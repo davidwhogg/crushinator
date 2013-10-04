2a696e4563cbcd012a9e8d56748e8fd4446e4159
Ross Fadely <rossfadely@gmail.com>
2013-10-04 14:54:48 +0000

    working newfirm sed generator

--- code/make_newfirm_bc03_seds.py
+++ code/make_newfirm_bc03_seds.py
@@ -2,28 +2,31 @@ import os
 import subprocess
 import numpy as np
 
+# bc03 library directory
+library_dir = '../external/bc03/models/Padova1994/chabrier/'
+
+# newfirm data file
 filename = '../data/newfirm/cosmos-1.deblend.sps/' + \
     'cosmos-1.bc03.del.deblend.v5.1.fout'
 
+# read data
 parms = np.loadtxt(filename)
 ind = np.where(parms[:, 1] != -1)[0]
 parms = parms[ind]
 print 'There are %d galaxies with measured parameters' % \
     ind.size
 
-# silly bc03 cant find library outside directory
-os.system('cp ../external/bc03/models/Padova1994/chabrier/*62*ssp.ised .')
-
 # A_v = 1.086 * tau_v
-N = parms.shape[0]
+N = parms.shape[0]*0 + 1
 for i in range(N):
     ident, z, tau = parms[i, :3]
     metal, age, Av = parms[i, 3:6]
 
+    # only using the 'm62' bc03
     assert metal == 0.02
 
     # input for csp_galaxev
-    commands = 'bc2003_hr_m62_chab_ssp.ised\n'
+    commands = library_dir + 'bc2003_hr_m62_chab_ssp.ised\n'
     commands += 'Y\n%0.3f\n'  % (Av / 1.086)
     commands += '0.3\n4\n%0.2f\n20.\n' % 10. ** (age - 9.)
     commands += 'out\n\n'
@@ -43,7 +46,5 @@ for i in range(N):
                          stdin=subprocess.PIPE)
     p.communicate(input=commands)
 
+    # cleanup
     os .system('rm out*')
-
-# cleanup
-os.system('rm *ised')
