cdc1b6dc16247834f4deb0154f05b46251e57fe5
Ross Fadely <rossfadely@gmail.com>
2013-10-04 14:47:06 +0000

    working newfirm sed generator

--- code/make_newfirm_bc03_seds.py
+++ code/make_newfirm_bc03_seds.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import numpy as np
 
 filename = '../data/newfirm/cosmos-1.deblend.sps/' + \
@@ -11,14 +12,38 @@ print 'There are %d galaxies with measured parameters' % \
     ind.size
 
 # silly bc03 cant find library outside directory
-os.system('cp ../external/bc03/models/Padova1994/chabrier/*62*ised .')
+os.system('cp ../external/bc03/models/Padova1994/chabrier/*62*ssp.ised .')
 
 # A_v = 1.086 * tau_v
-N = 30
+N = parms.shape[0]
 for i in range(N):
-    if parms[i, 1] != -1:
-        ident, z, tau = parms[i, :3]
-        metal, age, Av = parms[i, 3:6]
-        
+    ident, z, tau = parms[i, :3]
+    metal, age, Av = parms[i, 3:6]
+
+    assert metal == 0.02
+
+    # input for csp_galaxev
+    commands = 'bc2003_hr_m62_chab_ssp.ised\n'
+    commands += 'Y\n%0.3f\n'  % (Av / 1.086)
+    commands += '0.3\n4\n%0.2f\n20.\n' % 10. ** (age - 9.)
+    commands += 'out\n\n'
+
+    # run galaxev
+    p = subprocess.Popen('../external/bc03/src/csp_galaxev', 
+                         stdin=subprocess.PIPE)
+    p.communicate(input=commands)
+
+    # input for galaxev
+    commands = 'out.ised\n1200, 50000, 7000, 1, 0\n'
+    commands += '0.3\n' % 10. ** (age - 9.)
+    commands += '../data/newfirm/cosmos_seds/sed_%d.dat\n' % int(ident)
+
+    # run galaxev to get sed
+    p = subprocess.Popen('../external/bc03/src/galaxevpl', 
+                         stdin=subprocess.PIPE)
+    p.communicate(input=commands)
+
+    os .system('rm out*')
+
 # cleanup
 os.system('rm *ised')
